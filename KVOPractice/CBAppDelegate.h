048245860cc738e94bba50f1b15426b5d59a72a2
jimzhai <jszhaishuai@gmail.com>
2014-06-15 16:30:56 +0800

    完工

---
+++ KVOPractice/CBAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CBAppDelegate.h
+//  KVOPractice
+//
+//  Created by jimzhai on 6/14/14.
+//  Copyright (c) 2014 zhaishuai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CBAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ KVOPracticeTests/KVOPracticeTests.m
@@ -0,0 +1,34 @@
+//
+//  KVOPracticeTests.m
+//  KVOPracticeTests
+//
+//  Created by jimzhai on 6/14/14.
+//  Copyright (c) 2014 zhaishuai. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface KVOPracticeTests : XCTestCase
+
+@end
+
+@implementation KVOPracticeTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
